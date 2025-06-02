866d40f9c310b855618a9a65158fe51a83dad756
razzaksr <razzaksr@gmail.com>
2025-06-02 12:43:52 +0530

    project setup

---
+++ electro-phase-two/src/main/java/mandya/two/electro_phase_two/ElectroPhaseTwoApplication.java
@@ -0,0 +1,13 @@
+package mandya.two.electro_phase_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroPhaseTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroPhaseTwoApplication.class, args);
+	}
+
+}
---
+++ electro-phase-two/src/test/java/mandya/two/electro_phase_two/ElectroPhaseTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package mandya.two.electro_phase_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroPhaseTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
